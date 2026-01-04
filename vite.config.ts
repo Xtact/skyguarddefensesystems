9c7e9f0ed543ae46b020ae4dacf4384c3cc3daff
El Guapo <colbyfitch@yahoo.com>
2026-01-03 23:09:09 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  server: {
+    host: true, // This allows the Netlify preview server to "see" the port
+    port: 5173,
+    strictPort: true,
+  }
+})
