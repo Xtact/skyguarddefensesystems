49f26e99717603965c1eff522aeb23594fcf88cb
El Guapo <colbyfitch@yahoo.com>
2026-06-08 17:44:55 -0600

    4 files

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    host: true, // This allows the Netlify preview server to "see" the port
-    port: 5173,
-    strictPort: true,
-  }
-})
